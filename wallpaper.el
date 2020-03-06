7ed928223401d39fc6bfbf35e1f17d41ba724a87
Farlado <stultasergalo@gmail.com>
2020-03-06 18:45:45 -0500

    Push version to 1.0.0

--- wallpaper.el
+++ wallpaper.el
@@ -5,7 +5,7 @@
 ;; Author: Farlado <farlado@sdf.org>
 ;; URL: https://github.com/farlado/emacs-wallpaper
 ;; Keywords: unix, wallpaper, extensions
-;; Package-Version: 0.0.1
+;; Package-Version: 1.0.0
 ;; Package-Requires: ((emacs "25.1"))
 
 ;; This file is not part of GNU Emacs.
